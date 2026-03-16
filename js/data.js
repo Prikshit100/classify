f6abf516432265b0f3cfa8fd8a7b2ed93e1b3103
Prikshit100 <prikshit9031@gmail.com>
2026-03-16 19:53:38 +0530

    Update data.js

--- js/data.js
+++ js/data.js
@@ -1,1 +1,30 @@
+const institutes = [
 
+{
+name: "Allen Career Institute",
+exam: ["JEE","NEET"],
+city: "Kota",
+rating: 4.6,
+reviews: 320,
+students: 1200
+},
+
+{
+name: "Aakash Institute",
+exam: ["NEET","JEE"],
+city: "Delhi",
+rating: 4.3,
+reviews: 210,
+students: 950
+},
+
+{
+name: "Resonance",
+exam: ["JEE"],
+city: "Kota",
+rating: 4.4,
+reviews: 180,
+students: 800
+}
+
+];
